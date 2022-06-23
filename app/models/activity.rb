a9925c7c52fe21138537df5f153b8a872fdebf08
Francisca Montecinos <franmontecinosoportus@gmail.com>
2022-06-23 21:23:15 +1000

    Adding edit options to activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,7 @@ class Activity < ApplicationRecord
   has_many :bookings
 
   validates :title, :description, :price, :level, :location, :group_size, presence: true
-  validates :price, :group_size, numericality: { only_integer: true }
+  validates :price, numericality: { only_float: true }
+  validates :group_size, numericality: { only_integer: true }
   validates :level, inclusion: { in: ['Beginner', 'Intermediate', 'Advance'] }
 end
