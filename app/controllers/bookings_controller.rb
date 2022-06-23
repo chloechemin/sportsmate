65880eab3a4a90eedfcdfffe1bcfe8c231b21716
Chloe Chemin <chloechemin4@gmail.com>
2022-06-23 19:20:39 +0930

    controllers and views done for new and create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,29 @@
 class BookingsController < ApplicationController
 
+  # def index
+  #   @booking = Booking.all
+  # end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  # def show
+  #   @booking = Booking.find(params[:id])
+  # end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :status, :spots_required, :user_id, :activity_id)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :activity
   belongs_to :user
+
+  validates :start_time, :end_time, :status, :spots_required, presence: true
+  validates :spots_required, numericality: { only_integer: true }
+  validates :status, inclusion: { in: ['Not confirmed', 'Confirmed'] }
 end
