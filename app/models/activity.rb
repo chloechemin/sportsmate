ed0ada8d033dbae5e841f521efefab55a49dceb7
Chloe Chemin <chloechemin4@gmail.com>
2022-06-25 11:25:19 +0930

    Fixed the capital letters in levels for seeds

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,5 +4,5 @@ class Activity < ApplicationRecord
 
   validates :title, :description, :price, :level, :location, :group_size, presence: true
   validates :price, :group_size, numericality: { only_integer: true }
-  validates :level, inclusion: { in: ['Beginner', 'Intermediate', 'Advance'] }
+  validates :level, inclusion: { in: ['beginner', 'intermediate', 'advance'] }
 end
