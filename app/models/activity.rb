34ab2da8afd9b8021b4377b26ea8969ebd524151
Chloe Chemin <chloechemin4@gmail.com>
2022-06-25 11:59:09 +0930

    Levels in seeds have been capitalised

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,6 +5,6 @@ class Activity < ApplicationRecord
   validates :title, :description, :price, :level, :location, :group_size, presence: true
   validates :price, numericality: { only_float: true }
   validates :group_size, numericality: { only_integer: true }
-  validates :level, inclusion: { in: ['beginner', 'intermediate', 'advance'] }
+  validates :level, inclusion: { in: ['Beginner', 'Intermediate', 'Advance'] }
 
 end
